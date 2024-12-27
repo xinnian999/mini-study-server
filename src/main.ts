062da6f35c8e7c3bd09707e032e34bd3e1c4f396
hyl <3307378337@qq.com>
2024-12-27 08:51:53 +0800

    ip

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {networkInterfaces} from 'os'
+ 
+function getLocalIP() {
+  const interfaces = networkInterfaces();
+  for (const key in interfaces) {
+    const iface = interfaces[key];
+    for (let i = 0; i < iface.length; i++) {
+      const alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+        return alias.address;
+      }
+    }
+  }
+  return null;
+}
+
+console.log(getLocalIP());
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
