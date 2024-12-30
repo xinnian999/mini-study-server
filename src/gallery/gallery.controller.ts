70e5cfaa805a34e6131f3bd49437fe0c3f927107
v_huyilin <v_huyilin@baidu.com>
2024-12-30 18:28:02 +0800

    feat gallery

--- src/gallery/gallery.controller.ts
+++ src/gallery/gallery.controller.ts
@@ -1,18 +1,20 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { GalleryService } from './gallery.service';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('gallery')
 export class GalleryController {
   constructor(private readonly galleryService: GalleryService) {}
 
   @Get('list')
+  @UseGuards(AuthGuard)
   list(@Query('type') type: string) {
     return this.galleryService.findAll(type);
   }
 
   @Post('delete')
+  @UseGuards(AuthGuard)
   delete(@Body() body: Record<string, number[]>) {
-    
     return this.galleryService.delete(body.ids);
   }
 }
